51432c22c842ad02728235919e1bcadf533898eb
JoldoshAjimudunov <joldoshbek8856@gmail.com>
2023-03-06 18:56:58 +0600

    todo2

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,9 +1,22 @@
+import Button from "@mui/material/Button";
 import React, { useState } from "react";
 import { IPost } from "../models/IPost";
 import { postAPI } from "../services/PostService";
 import PostItem from "./PostItem";
+import "../style/post.css";
 
-const PostContainer = () => {
+interface IState {
+  list: {
+    todoitem: string;
+  };
+}
+
+const PostContainer: React.FC = () => {
+  const [state, setState] = useState<IState>({
+    list: {
+      todoitem: "",
+    },
+  });
   const [limit, setLimit] = useState(100);
   const {
     data: posts,
@@ -15,7 +28,7 @@ const PostContainer = () => {
   const [deletePost, {}] = postAPI.useDeletePostMutation();
 
   const handleCreate = async () => {
-    const title = prompt();
+    const title = JSON.stringify(state.list);
     await createPost({ title, body: title } as any);
   };
 
@@ -26,11 +39,30 @@ const PostContainer = () => {
   const handleUpdate = (post: IPost) => {
     updatePost(post);
   };
+  ///////////////
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setState({
+      list: {
+        ...state.list,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  ///////////////
   return (
     <div>
       <div className="post__list">
-        <button onClick={handleCreate}>add post</button>
+        <input
+          type="text"
+          value={state.list.todoitem}
+          onChange={handleChange}
+          name="todoitem"
+        />
+        <Button variant="outlined" onClick={handleCreate}>
+          add post
+        </Button>
         {isLoading && <h1>loading in proccess</h1>}
         {error && <h1>error</h1>}
         {posts &&
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,5 +1,9 @@
+import { Card, Container } from "@mui/material";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import React, { FC } from "react";
 import { IPost } from "../models/IPost";
+import "../style/post.css";
 
 interface PostItemProps {
   post: IPost;
@@ -17,9 +21,33 @@ const PostItem: FC<PostItemProps> = ({ post, remove, update }) => {
     update({ ...post, title });
   };
   return (
-    <div className="post" onClick={handleUpdate}>
-      {post.id}. {post.title}
-      <button onClick={handleRemove}>delete</button>
+    <div className="post">
+      <Card sx={{ width: 505, m: 2 }}>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={2}
+        >
+          {post.title}
+          <div>
+            <Button
+              sx={{ width: "20px", height: "30px", fontSize: "12px" }}
+              variant="contained"
+              onClick={handleUpdate}
+            >
+              edit
+            </Button>
+            <Button
+              sx={{ width: "20px", height: "30px", fontSize: "12px" }}
+              variant="contained"
+              onClick={handleRemove}
+            >
+              delete
+            </Button>
+          </div>
+        </Stack>
+      </Card>
     </div>
   );
 };
